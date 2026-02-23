d42e6ffda855169ce9d49b26efb19cf9eca656e7
karneva <kimsk3568@gmail.com>
2026-02-23 22:41:44 +0900

    백준 2667 풀이

--- boj/2667.py
+++ boj/2667.py
@@ -1,11 +1,42 @@
 # 백준 2667번: 단지번호붙이기
 
-import sys
-sys.stdin = open("input.txt")
+def dfs(x, y):
+    stack = [(x, y)]
+    field[x][y] = 0
+    cnt = 1
+    while stack:
+        sx, sy = stack.pop()
+
+        for dx, dy in delta:
+            nx, ny = sx+dx, sy+dy
+            if 0 <= nx < N and 0 <= ny < N:
+                if field[nx][ny] == 1:
+                    field[nx][ny] = 0
+                    stack.append((nx, ny))
+                    cnt += 1
+
+    return cnt
+
 
 N = int(input())
 
-filed = [list(map(int, input())) for i in range(N)]
+field = [list(map(int, input().strip())) for _ in range(N)]
 
-delta = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+delta = [(1,0), (0,1), (-1,0), (0,-1)]
 
+# dfs로 돌면서 0으로 만들고 개수를 세서 리스트에 넣기
+
+result = []
+
+for i in range(N):
+    for j in range(N):
+        if field[i][j] == 1:
+            cnt = dfs(i, j)
+            result.append(cnt)
+
+result.sort()
+
+l = len(result)
+print(len(result))
+for x in result:
+    print(x)
