9cb2e4e66d80fd760defc8f398a6c09826e12a37
karneva <kimsk3568@naver.com>
2026-02-24 10:38:45 +0900

    백준 2178 풀이

--- boj/2178.py
+++ boj/2178.py
@@ -10,3 +10,16 @@ delta = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 sx, sy, ex, ey = 0, 0, N-1, M-1
 
+queue = deque([(sx, sy)])
+
+while queue:
+    x, y = queue.popleft()
+
+    for dx, dy in delta:
+        nx, ny = x+dx, y+dy
+
+        if 0 <= nx < N and 0 <= ny < M and field[nx][ny] == 1:
+            queue.append((nx, ny))
+            field[nx][ny] = field[x][y] + 1
+
+print(field[ex][ey])
