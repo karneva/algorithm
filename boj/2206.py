f53570a7d435fb1526f7a106a5fc893300162d55
karneva <kimsk3568@naver.com>
2026-02-27 17:35:37 +0900

    백준 2206 풀이

--- boj/2206.py
+++ boj/2206.py
@@ -3,4 +3,35 @@
 import sys
 from collections import deque
 
-sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+field = [list(map(int, input().strip())) for _ in range(N)]
+
+dist = [[[0]*2 for _ in range(M)] for _ in range(N)]
+dist[0][0][0] = 1
+
+delta = [(1,0), (0,1), (-1,0), (0,-1)]
+
+queue = deque([(0, 0, 0)])
+
+while queue:
+    x, y, smash = queue.popleft()
+
+    if x == N-1 and y == M-1:
+        print(dist[x][y][smash])
+        sys.exit()
+
+    for dx, dy in delta:
+        nx, ny = x+dx, y+dy
+
+        if 0 <= nx < N and 0 <= ny < M:
+            if field[nx][ny] == 0 and dist[nx][ny][smash] == 0:
+                dist[nx][ny][smash] = dist[x][y][smash] + 1
+                queue.append((nx, ny, smash))
+            elif field[nx][ny] == 1 and smash == 0 and dist[nx][ny][smash] == 0:
+                dist[nx][ny][1] = dist[x][y][smash] + 1
+                queue.append((nx, ny, 1))
+
+print(-1)
