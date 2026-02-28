d546229503bd2872b2b9cb5574f57aa5045d45fa
karneva <kimsk3568@naver.com>
2026-02-28 23:59:24 +0900

    백준 7569

---
+++ boj/7569.py
@@ -0,0 +1,37 @@
+# 백준 7569번: 토마토
+
+import sys
+from collections import deque
+
+sys.stdin = open("input.txt")
+
+M, N, H = map(int, input().split())
+
+box = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+
+delta = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, 0, 1), (0, -1, 0), (0, 0, -1)]
+
+tomato = deque([])
+
+for z in range(H):
+    for x in range(N):
+        for y in range(M):
+            if box[z][x][y] == 1:
+                tomato.append((z, x, y))
+
+cnt = 0
+
+while tomato:
+    cz, cx, cy = tomato.popleft()
+
+    for dz, dx, dy in delta:
+        nz, nx, ny = cz+dz, cx+dx, cy+dy
+
+        if 0 <= nz < H and 0 <= nx < N and 0 <= ny < M:
+            if box[nz][nx][ny] == 0:
+                box[nz][nx][ny] = 1
+                tomato.append((nz, nx, ny))
+
+    cnt += 1
+
+print(cnt)
