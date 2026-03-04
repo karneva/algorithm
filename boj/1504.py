1759ee246acc40d31d8b42577b78b86a46025fe7
karneva <kimsk3568@gmail.com>
2026-03-04 22:14:44 +0900

    벡준 1504 풀이

--- boj/1504.py
+++ boj/1504.py
@@ -2,31 +2,43 @@
 import sys
 import heapq
 
-sys.stdin = open("input.txt")
+def dijkstra(start):
+    dist = [10 ** 18] * (N + 1) 
+    dist[start] = 0
+
+    pq = [(0, start)]
+
+    while pq:
+        cur_dist, now = heapq.heappop(pq)
+        if cur_dist != dist[now]:
+            continue
+
+        for nxt, w in graph[now]:
+            nxt_dist = cur_dist + w
+            if nxt_dist < dist[nxt]:
+                dist[nxt] = nxt_dist
+                heapq.heappush(pq, (nxt_dist, nxt))
+
+    return dist
 
 N, E = map(int, input().split())
 
-graph = [[] for _ in range(E+1)]
+graph = [[] for _ in range(N+1)]
 
 for _ in range(E):
     u, v, w = map(int, input().split())
     graph[u].append((v, w))
+    graph[v].append((u, w))
 
 v1, v2 = map(int, input().split())
 
-dist1 = [10**18] * (N+1)  # v1, v2 최소 구하고
-dist1[1] = 0
+dist1 = dijkstra(1)
+distv1 = dijkstra(v1)
+distv2 = dijkstra(v2)
 
-pq1 = [(0, 1)]
+ans = min(dist1[v1]+distv1[v2]+distv2[N], dist1[v2]+distv1[N]+distv2[v1])
 
-while pq1:
-    cur_dist, now = heapq.heappop(pq1)
-
-    for nxt, w in graph[now]:
-        nxt_dist = cur_dist + w
-        if nxt_dist < dist1[nxt]:
-            dist1[nxt] = nxt_dist
-            heapq.heappush(pq1, (nxt_dist, nxt))
-
-distv1 = [10**18] * (N+1)  # 반대 방향으로 최소 구하고
-distv2 = [10**18] * (N+1)  # N으로 최소 구하고
+if ans >= 10**18:
+    print(-1)
+else:
+    print(ans)
